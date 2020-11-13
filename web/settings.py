02c87048f5c35bb4829e8b3e401984f1812ac04a
bivek1 <bivekdhakal18@gmail.com>
2020-11-13 10:52:55 +0545

    gg

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_hbohd80ibk1nmh_+faghon-5*$gr0v=a+kmdjy7d6dmf4%@_3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['128.199.102.53']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
