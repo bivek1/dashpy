7c39a1d3fe4b8c35cb407547ad030ba0b14eb7b4
bivek1 <bivekdhakal18@gmail.com>
2020-12-03 21:50:21 +0545

    final1

--- web/settings.py
+++ web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_hbohd80ibk1nmh_+faghon-5*$gr0v=a+kmdjy7d6dmf4%@_3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['104.248.54.67']
 
